f35baa05213664c9b85e92e4e1eb598a0005c9ed
seiya <skkasans@gmail.com>
2020-11-29 20:33:22 +0900

    ハローと応答させる

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api'], function() {
+    // LineからのWebhookを受信
+    Route::post('/line/webhook', 'LineWebhookController@webhook')->name('line.webhook');
+});
